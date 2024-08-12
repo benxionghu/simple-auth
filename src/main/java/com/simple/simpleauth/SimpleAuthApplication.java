5aaec571c16c17e1d0c75b27e61c65d009ca658b
benxiong hu <1301536601@qq.com>
2024-08-12 16:49:33 +0800

    init project

---
+++ src/main/java/com/simple/simpleauth/SimpleAuthApplication.java
@@ -0,0 +1,16 @@
+package com.simple.simpleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 初始化应用程序类
+ */
+@SpringBootApplication
+public class SimpleAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAuthApplication.class, args);
+    }
+
+}
